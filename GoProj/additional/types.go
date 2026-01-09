14ebf80bd7653e2b7054e92088b58792f9aa16e1
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2026-01-09 23:16:15 +0700

    Initial commit

---
+++ GoProj/additional/types.go
@@ -0,0 +1,24 @@
+package additional
+
+import "fmt"
+
+func Printtext() {
+	var x1 string = "int, int8, in16, int32, int64"       // 0
+	var x2 string = "uint, uint8, uint16, uint32, uint64" // 0
+	var x3 string = "float32, float64"                    // 0.0
+	var x4 string = "string"                              // ""
+	var x5 string = "bool"                                // false
+	var x6 string = "complex64, complex128"               // (0+0i)
+	fmt.Println("Целочисленные типы ->", x1, x2)
+	fmt.Println("Вещественные типы ->", x3)
+	fmt.Println("Строковый тип ->", x4)
+	fmt.Println("Логический тип ->", x5)
+	fmt.Println("Комлексный тип ->", x6)
+
+	fmt.Println("\nВ языке используется статическая типизация")
+	fmt.Println("Типы в Go делятся на четыре категориии")
+	fmt.Println("Простые (фундаментальные)")
+	fmt.Println("Составные (агрегированные) - массивы и структуры")
+	fmt.Println("Ссылочные - указатели, срезы, отображения, функции, каналы")
+	fmt.Println("Интерфейсы")
+}
