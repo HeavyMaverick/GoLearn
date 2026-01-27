d3fa32b55b27267d7c646520eff9b24f2b0e4bf8
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2026-01-27 16:58:05 +0700

    Solution for task

--- gp2/main.go
+++ gp2/main.go
@@ -1,5 +1,19 @@
 package main
 
 func main() {
-
+	mainSeconds := 0
+	totalWorkSecond := 0
+	timeNow := time.Now()
+	ch := make(chan int, 100)
+	for range 100 {
+		go func() {
+			ch <- randomWait()
+		}()
+	}
+	for range 100 {
+		totalWorkSecond += <-ch
+	}
+	mainSeconds = int(time.Since(timeNow).Seconds())
+	fmt.Println("main", mainSeconds)
+	fmt.Println("total", totalWorkSecond)
 }
