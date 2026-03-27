0402a52c92cf048b2193026c75a9c6e5405b1019
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2026-03-27 15:41:57 +0700

    Create 1.go

---
+++ Leet/1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func main() {
+	isPalindrome(121)
+}
+
+func isPalindrome(x int) bool {
+	var builder strings.Builder
+	var d string
+	s := strconv.Itoa(x)
+	for i := len(s) - 1; i >= 0; i-- {
+		builder.WriteString(string(s[i]))
+	}
+	d = builder.String()
+	if s == d {
+		return true
+	} else {
+		return false
+	}
+}
